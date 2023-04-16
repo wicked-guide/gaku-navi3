acd054a5f47850c0f7cab8b0ce91223ff7e27dd1
wicked <wicked.wonder.world@gmail.com>
2023-04-16 17:10:53 +0900

    github-pageで公開

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: './'
-})
+  // publicPath: './'
+  publicPath: process.env.NODE_ENV === "production" ? "/gaku-navi/" : "./",
+});
